9f5ea19eeb45f3fa8e147e44607ca5fca54c880d
Thomas Chacko <106021054+thomas-chacko@users.noreply.github.com>
2025-03-01 14:53:02 +0530

    db connectio

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const connectionString = process.env.MONGO_URI;
 
 mongoose
-  .connect(connectionString)
-  .then(() => {
-    console.log("mongo db connected");
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
